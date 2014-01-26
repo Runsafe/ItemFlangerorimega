ef8e6ac5224836d3cbd9052c893fd9fcb790f577
Morten Nilsen <morten@runsafe.no>
2014-01-26 23:07:49 +0100

    Upstream API changes

---
+++ src/no/runsafe/itemflangerorimega/commands/EnchantArgument.java
@@ -0,0 +1,48 @@
+package no.runsafe.itemflangerorimega.commands;
+
+import com.google.common.collect.ImmutableList;
+import no.runsafe.framework.api.command.ICommandExecutor;
+import no.runsafe.framework.api.command.argument.ITabComplete;
+import no.runsafe.framework.api.command.argument.IValueExpander;
+import no.runsafe.framework.api.command.argument.RequiredArgument;
+import no.runsafe.framework.api.player.IPlayer;
+import no.runsafe.itemflangerorimega.bows.CustomBowEnchantHandler;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class EnchantArgument extends RequiredArgument implements ITabComplete, IValueExpander
+{
+	public EnchantArgument(CustomBowEnchantHandler handler)
+	{
+		super("enchant");
+		this.handler = handler;
+	}
+
+	@Override
+	public List<String> getAlternatives(IPlayer executor, String partial)
+	{
+		Set<String> options = handler.getAvailableEnchants();
+		if (partial == null || partial.isEmpty())
+			return ImmutableList.copyOf(options);
+
+		String match = partial.toLowerCase();
+		List<String> alternatives = new ArrayList<String>(options.size());
+		for (String option : options)
+			if (option.toLowerCase().startsWith(match))
+				alternatives.add(option);
+		return alternatives;
+	}
+
+	@Nullable
+	@Override
+	public String expand(ICommandExecutor context, @Nullable String value)
+	{
+		List<String> options = getAlternatives((IPlayer) context, value);
+		return options.isEmpty() || options.size() > 1 ? null : options.get(0);
+	}
+
+	private CustomBowEnchantHandler handler;
+}
--- src/no/runsafe/itemflangerorimega/commands/EnchantBow.java
+++ src/no/runsafe/itemflangerorimega/commands/EnchantBow.java
@@ -1,19 +1,11 @@
 package no.runsafe.itemflangerorimega.commands;
 
-import com.google.common.collect.ImmutableList;
 import no.runsafe.framework.api.command.argument.IArgumentList;
-import no.runsafe.framework.api.command.argument.RequiredArgument;
 import no.runsafe.framework.api.command.player.PlayerCommand;
 import no.runsafe.framework.api.player.IPlayer;
 import no.runsafe.framework.minecraft.Item;
 import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
 import no.runsafe.itemflangerorimega.bows.CustomBowEnchantHandler;
-import org.apache.commons.lang.StringUtils;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.List;
-import java.util.Set;
 
 public class EnchantBow extends PlayerCommand
 {
@@ -21,34 +13,22 @@ public class EnchantBow extends PlayerCommand
 	{
 		super(
 			"enchantbow", "Enchants a bow using a magical custom enchant.", "runsafe.flangerorimega.enchant.bows",
-			new RequiredArgument("enchant")
+			new EnchantArgument(handler)
 		);
 		this.handler = handler;
-	}
-
-	@Nullable
-	@Override
-	public List<String> getParameterOptions(@Nonnull String parameter)
-	{
-		return parameter.equals("enchant") ? ImmutableList.copyOf(handler.getAvailableEnchants()) : null;
 	}
 
 	@Override
 	public String OnExecute(IPlayer executor, IArgumentList parameters)
 	{
-		String enchantType = parameters.get("enchant").toLowerCase();
-		Set<String> enchantTypes = this.handler.getAvailableEnchants();
-		if (enchantTypes.contains(enchantType))
+		String enchantType = parameters.get("enchant");
+		RunsafeMeta item = executor.getItemInHand();
+		if (item != null && item.is(Item.Combat.Bow))
 		{
-			RunsafeMeta item = executor.getItemInHand();
-			if (item != null && item.is(Item.Combat.Bow))
-			{
-				this.handler.enchantBow(item, this.handler.getEnchant(enchantType));
-				return "&2Your bow has been enchanted with wizardry.";
-			}
-			return "&cYou are not holding a bow.";
+			this.handler.enchantBow(item, this.handler.getEnchant(enchantType));
+			return "&2Your bow has been enchanted with wizardry.";
 		}
-		return "Valid enchant types: " + StringUtils.join(enchantTypes, ", ");
+		return "&cYou are not holding a bow.";
 	}
 
 	private CustomBowEnchantHandler handler;
