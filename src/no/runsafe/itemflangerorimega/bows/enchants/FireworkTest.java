9269a6beb6f479330f6ecba91aa3d1bc0b09deb4
Kruithne <kruithne@gmail.com>
2014-02-17 07:34:17 +0000

    Use Bukkit stuff because our firework builder is not working right.

--- src/no/runsafe/itemflangerorimega/bows/enchants/FireworkTest.java
+++ src/no/runsafe/itemflangerorimega/bows/enchants/FireworkTest.java
@@ -1,15 +1,36 @@
 package no.runsafe.itemflangerorimega.bows.enchants;
 
+import no.runsafe.framework.api.ILocation;
+import no.runsafe.framework.api.entity.IEntity;
 import no.runsafe.framework.api.entity.ILivingEntity;
-import no.runsafe.framework.minecraft.Firework;
+import no.runsafe.framework.minecraft.entity.ProjectileEntity;
+import no.runsafe.framework.minecraft.entity.RunsafeEntity;
 import no.runsafe.itemflangerorimega.bows.CustomBowEnchant;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.entity.Firework;
+import org.bukkit.inventory.meta.FireworkMeta;
 
 public class FireworkTest extends CustomBowEnchant
 {
 	@Override
 	public boolean onArrowShoot(ILivingEntity entity)
 	{
-		Firework.Random().Power(5).Fire(entity.getLocation());
+		ILocation location = entity.getLocation();
+
+		if (location != null)
+		{
+			IEntity fireworkEntity = location.getWorld().spawnCreature(location, ProjectileEntity.Firework.getName());
+			Firework firework = (Firework) ((RunsafeEntity) fireworkEntity).getRaw();
+
+			FireworkMeta meta = firework.getFireworkMeta();
+			FireworkEffect effect = FireworkEffect.builder().with(FireworkEffect.Type.BALL_LARGE).withColor(Color.OLIVE).withFade(Color.RED).flicker(true).trail(true).build();
+
+			meta.addEffect(effect);
+			meta.setPower(5);
+			firework.setFireworkMeta(meta);
+		}
+
 		return false;
 	}
 
