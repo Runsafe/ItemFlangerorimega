721039c92e2fef3be86aa78d9c83c1a4481c242a
Kruithne <kruithne@gmail.com>
2014-04-22 21:04:40 +0100

    Delete unused firework test.

--- src/no/runsafe/itemflangerorimega/Plugin.java
+++ src/no/runsafe/itemflangerorimega/Plugin.java
@@ -10,7 +10,6 @@ import no.runsafe.itemflangerorimega.armour.enchants.PathOfFrost;
 import no.runsafe.itemflangerorimega.armour.enchants.Resilience;
 import no.runsafe.itemflangerorimega.bows.CustomBowEnchantHandler;
 import no.runsafe.itemflangerorimega.bows.enchants.ExplosiveCharge;
-import no.runsafe.itemflangerorimega.bows.enchants.FireworkTest;
 import no.runsafe.itemflangerorimega.commands.EnchantArmour;
 import no.runsafe.itemflangerorimega.commands.EnchantBow;
 import no.runsafe.itemflangerorimega.commands.EnchantTool;
@@ -30,7 +29,6 @@ public class Plugin extends RunsafePlugin
 
 		// Bow Enchants
 		addComponent(ExplosiveCharge.class);
-		addComponent(FireworkTest.class);
 
 		// Armour enchants
 		addComponent(Resilience.class);
