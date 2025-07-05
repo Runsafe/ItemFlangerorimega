cce2f873eb0913795d96e28f8e805f16d66dd791
DeveloperDeborah <71165709+DeveloperDeborah@users.noreply.github.com>
2025-07-05 17:22:44 -0400

    Make KaosFire enchant not deal explosion damage

--- src/no/runsafe/itemflangerorimega/bows/enchants/KaosFire.java
+++ src/no/runsafe/itemflangerorimega/bows/enchants/KaosFire.java
@@ -2,6 +2,8 @@ package no.runsafe.itemflangerorimega.bows.enchants;
 
 import no.runsafe.framework.api.ILocation;
 import no.runsafe.framework.api.entity.projectiles.IProjectile;
+import no.runsafe.framework.minecraft.Item;
+import no.runsafe.framework.minecraft.Sound;
 import no.runsafe.itemflangerorimega.bows.CustomBowEnchant;
 
 public class KaosFire extends CustomBowEnchant
@@ -25,8 +27,27 @@ public class KaosFire extends CustomBowEnchant
 		if (loc == null)
 			return;
 
-		loc.getWorld().createExplosion(loc, 10, true, false);
+		int radius = 3; // Scalable sphere
+		int radiusSquared = radius * radius;
 
+		for (double x = -radius; x <= radius; x++)
+			for (double y = -radius; y <= radius; y++)
+				for (double z = -radius; z <= radius; z++)
+				{
+					// Checks if the point falls inside the spherical radius
+					if (x * x + y * y + z * z > radiusSquared)
+						continue;
+
+					ILocation fireLoc = loc.clone();
+					fireLoc.offset(x, y, z);
+
+					if (!fireLoc.getBlock().isAir())
+						continue;
+
+					fireLoc.getBlock().set(Item.Unavailable.Fire);
+				}
+
+		loc.playSound(Sound.Environment.Explode);
 		projectile.remove();
 	}
 }
